6aa2aa430a2b29c0e14138a47902951da58f6030
Helton Barbosa <heltonbs@gmail.com>
2024-03-22 21:59:29 -0300

    Comentários e DocString

---
+++ Seção_03/aula_001.py
@@ -0,0 +1,11 @@
+# Permite escrever um comentário
+
+"""
+Esse é um exemplo de DocString.
+Utilizamos DocStrings para documentar o código.
+"""
+
+print(123) # Comentário naa frente
+# Comentáario abaixo
+print(456)
+
