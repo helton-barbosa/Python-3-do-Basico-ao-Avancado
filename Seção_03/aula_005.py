ee49404f3f92cdaabacbd5f5bece315d50fb284a
Helton Barbosa <heltonbs@gmail.com>
2024-03-22 22:29:32 -0300

    Tipo de dados: bool

---
+++ Seção_03/aula_005.py
@@ -0,0 +1,9 @@
+# Tipo de dado bool (booleano)
+# Ao questionar algo em um programa, só existem duas respostas possíveis:
+# sim (True) e não (False).
+# Existem vários operadores para questionar.
+# Dentre eles o ==, que é um operador lógico que questiona
+# se um valor é igual a outro valor
+print(10 == 10) # Sim -> True (verdadeiro)
+print(1 == 2) # Não -> False (falso)
+
