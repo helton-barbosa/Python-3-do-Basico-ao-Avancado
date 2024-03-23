b209b0997e1e6824e4b0197375f6e1241964ecb6
Helton Barbosa <heltonbs@gmail.com>
2024-03-22 22:11:35 -0300

    Tipo de dados: string

---
+++ Seção_03/aula_003.py
@@ -0,0 +1,19 @@
+# str ou string ou texto
+# São textos que estão dentro de aspas
+
+# Aspas simples
+print('Texto com aspas simples...')
+
+# Aspas duplas
+print("Texto com aspas duplas...")
+
+# Aspas dentro de aspas
+print("Meu nome é 'Helton'")
+print('Sou casado com "Rosilene"')
+
+# Escape
+print("Meu sobrenome é \"Barbosa\"")
+
+# r
+print(r"Helton \"Lampião\"")
+
