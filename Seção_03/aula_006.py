606643ea9e1ed5bd80865190bd4c9a6bd81f69e6
Helton Barbosa <heltonbs@gmail.com>
2024-03-22 22:40:24 -0300

    Coerção de tipos (typecasting) e básico de polimorfismo

---
+++ Seção_03/aula_006.py
@@ -0,0 +1,16 @@
+# Conversão de tipos ou Coerção de tipos
+# type convertion ou typecasting ou coercion
+# é o ato de converter um tipo de dado em outro tipo de dado
+# Tipos imutáveis e primitivos: int, str, float, bool
+
+print(1 + 1) # Polimorfismo - o sinal de mais (+), em tipo de dados numéricos
+# realiza a soma dos números
+
+print('H' + 'e' + 'l' + 't' + 'o' + 'n') # Polimorfismo - o sinal de mais (+),
+# em tipos de dados string, realiza a concatenação (junção) dos dados
+
+print(str(1) + '100')
+print(int('10') + 10)
+print(float(100))
+print(bool(1))
+
