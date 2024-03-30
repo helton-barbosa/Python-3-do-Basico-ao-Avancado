d7e08fcd7bedddd8a595c4f9489a866ac93d4207
Helton Barbosa <heltonbs@gmail.com>
2024-03-30 16:29:38 -0300

    Operações matemáticas básicas

---
+++ aula_009.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print(f'Adição: {adicao}')
+
+subtracao = 10 - 5
+print(f'Subtração: {subtracao}')
+
+multiplicacao = 10 * 10
+print(f'Multiplicação: {multiplicacao}')
+
+divisao = 10 / 2.2
+print(f'Divisão: {divisao}')
+
+divisao_inteira = 10 // 2.2
+print(f'Divisão Inteira: {divisao_inteira}')
+
+exponenciacao = 2 ** 12
+print(f'Exponenciação: {exponenciacao}')
+
+modulo = 55 % 2
+print(f'Módulo: {modulo}')  # Resto da divisão
