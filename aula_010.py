a4f80d3c9e8d6868a20dcb6ed6a7253a60f0ebbe
Helton Barbosa <heltonbs@gmail.com>
2024-03-30 16:33:24 -0300

    Concatenação (+) e repetição (*) com operadores aritméticos

---
+++ aula_010.py
@@ -0,0 +1,9 @@
+concatenacao_1 = 'a' + 'b' + 'c'
+print(concatenacao_1)
+concatenacao_2 = 'Helton' + ' ' + 'Barbosa'
+print(concatenacao_2)
+
+repeticao_1 = 'A' * 10
+print(repeticao_1)
+repeticao_2 = 'Bola' * 3
+print(repeticao_2)
