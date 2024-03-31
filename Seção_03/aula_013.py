bb516305bef4e827181486aa57572c5e050156f4
Helton Barbosa <heltonbs@gmail.com>
2024-03-31 09:08:23 -0300

    Uma introdução às f-strings (formatação de strings)

---
+++ Seção_03/aula_013.py
@@ -0,0 +1,8 @@
+nome = 'Helton Barbosa'
+altura = 1.79
+peso = 75
+imc = peso / altura ** 2
+valor = 125365.00
+
+print(f'{nome} tem {altura:.2f}m e pesa {peso}kg. Seu IMC é: {imc:.2f}.')
+print(f'Teste de valor em reais com vírgula: {valor:,.2f}')
