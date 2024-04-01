6a77743933805f65ee0547d11986437b6b981aaf
Helton Barbosa <heltonbs@gmail.com>
2024-03-31 21:34:45 -0300

    Corrigindo código

--- Seção_03/aula_005.py
+++ Seção_03/aula_005.py
@@ -4,6 +4,5 @@
 # Existem vários operadores para questionar.
 # Dentre eles o ==, que é um operador lógico que questiona
 # se um valor é igual a outro valor
-print(10 == 10) # Sim -> True (verdadeiro)
-print(1 == 2) # Não -> False (falso)
-
+print(10 == 10)  # Sim -> True (verdadeiro)
+print(1 == 2)  # Não -> False (falso)
