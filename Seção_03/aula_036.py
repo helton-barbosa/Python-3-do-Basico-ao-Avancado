4cb59a5b3d80c85d3d00261adb1176e314d9a48f
Helton Barbosa <heltonbs@gmail.com>
2024-04-01 22:22:53 -0300

    Operadores de atribuição com operadores aritméticos

---
+++ Seção_03/aula_036.py
@@ -0,0 +1,75 @@
+# Operadores de atribuição em Python são usados para atribuir valores a
+# variáveis. Eles permitem que você modifique o valor de uma variável com base
+# em sua valor atual, realizando uma operação aritmética em conjunto com a
+# atribuição. Aqui estão os operadores de atribuição com operadores aritméticos
+# em Python:
+
+# Atribuição Simples (=):
+# O operador de atribuição simples, =, é usado para atribuir um valor a uma
+# variável.
+
+# Exemplo:
+x = 10  # atribui o valor 10 à variável x
+
+# Adição e Atribuição (+=):
+# O operador += adiciona o valor à variável e atribui o resultado de volta à
+# mesma variável.
+
+# Exemplo:
+x = 5
+x += 3  # equivale a x = x + 3
+print(x)  # Saída: 8
+
+# Subtração e Atribuição (-=):
+# O operador -= subtrai o valor da variável e atribui o resultado de volta à
+# mesma variável.
+
+# Exemplo:
+x = 10
+x -= 4  # equivale a x = x - 4
+print(x)  # Saída: 6
+
+# Multiplicação e Atribuição (*=):
+# O operador *= multiplica o valor da variável pelo valor especificado e
+# atribui o resultado de volta à mesma variável.
+
+# Exemplo:
+x = 3
+x *= 2  # equivale a x = x * 2
+print(x)  # Saída: 6
+
+# Divisão e Atribuição (/=):
+# O operador /= divide o valor da variável pelo valor especificado e atribui
+# o resultado de volta à mesma variável.
+
+# Exemplo:
+x = 10
+x /= 5  # equivale a x = x / 5
+print(x)  # Saída: 2.0 (resultado da divisão como float)
+
+# Divisão de Inteiro e Atribuição (//=):
+# O operador //= divide o valor da variável pelo valor especificado, realizando
+# a divisão de inteiros, e atribui o resultado de volta à mesma variável.
+
+# Exemplo:
+x = 10
+x //= 3  # equivale a x = x // 3
+print(x)  # Saída: 3 (resultado da divisão como inteiro)
+
+# Módulo e Atribuição (%=):
+# O operador %= calcula o módulo do valor da variável pelo valor especificado e
+# atribui o resultado de volta à mesma variável.
+
+# Exemplo:
+x = 10
+x %= 3  # equivale a x = x % 3
+print(x)  # Saída: 1 (resto da divisão de 10 por 3)
+
+# Potência e Atribuição (**=):
+# O operador **= eleva o valor da variável à potência especificada e atribui o
+# resultado de volta à mesma variável.
+
+# Exemplo:
+x = 2
+x **= 3  # equivale a x = x ** 3
+print(x)  # Saída: 8 (2 elevado à potência 3)
