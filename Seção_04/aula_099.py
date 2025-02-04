bf777463833db5fa9e072f590ac2d48a9ccd8f39
Helton Barbosa <heltonbs@gmail.com>
2025-02-04 18:56:46 -0300

    159 __init__ é um arquivo de inicialização dos packages em Python

--- Seção_04/aula_099.py
+++ Seção_04/aula_099.py
@@ -1,6 +1,10 @@
 # from sys import path
-from aula_099_package.modulo import soma_do_modulo
+# from aula_099_package.modulo import soma_do_modulo
 
-# print(*path, sep='\n')
-print(soma_do_modulo(3, 7))
-print(__name__)
+# # print(*path, sep='\n')
+# print(soma_do_modulo(3, 7))
+# print(__name__)
+
+import aula_099_package
+
+print(aula_099_package.soma_do_modulo(2, 1))
---
+++ Seção_04/aula_099_package/__init__.py
@@ -0,0 +1,7 @@
+# print(f'Você importou {__name__}')
+
+
+# def dobra(x):
+#     return x * 2
+
+from .modulo import soma_do_modulo
