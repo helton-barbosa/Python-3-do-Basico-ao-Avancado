a9832ab4e52d725ef5b678823fde211e11d1ab7a
Helton Barbosa <heltonbs@gmail.com>
2025-05-09 18:47:59 -0300

    Log, LogFileMixin, LogPrintMixin e a união de tudo até aqui

--- Seção_05/aula_141/log.py
+++ Seção_05/aula_141/log.py
@@ -1,14 +1,27 @@
 # Abstração
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o mátdodo log')
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg} --> {self.__class__.__name__}')
+
+    def log_success(self, msg):
+        return self._log(f'Sucess: {msg} --> {self.__class__.__name__}')
 
 
 class LogFileMixing(Log):
-    def log(self, msg):
+    def _log(self, msg):
+        print(msg)
+
+
+class LogPrintMixing(Log):
+    def _log(self, msg):
         print(msg)
 
 
 if __name__ == '__main__':
-    l = LogFileMixing()
-    l.log('Teste')
+    lf = LogFileMixing()
+    lp = LogPrintMixing()
+    lf.log_error('Teste')
+    lp.log_success('Teste')
